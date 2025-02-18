8e13c7efcd3b44b8f99d6aa668b42e3770280215
Rosa van Wouwe <rosavanwouwe@hotmail.com>
2025-02-18 11:42:45 +0100

    Time updates every second
    Import moment js

--- source/script.js
+++ source/script.js
@@ -1,1 +1,22 @@
+function setClock() {
+  let amsterdam = document.querySelector("#amsterdam");
+  let amsterdamTime = amsterdam.querySelector(".time");
+  amsterdamTime.innerHTML = moment()
+    .tz("Europe/Amsterdam")
+    .format("HH:mm[<span>]:ss[</span>]");
+  let amsterdamDate = amsterdam.querySelector(".date");
+  amsterdamDate.innerHTML = moment().tz("Europe/Amsterdam").format("LL");
 
+  let buenosAires = document.querySelector("#buenos-aires");
+  let buenosAiresTime = buenosAires.querySelector(".time");
+  buenosAiresTime.innerHTML = moment()
+    .tz("America/Argentina/Buenos_Aires")
+    .format("HH:mm[<span>]:ss[</span>]");
+  let buenosAiresDate = buenosAires.querySelector(".date");
+  buenosAiresDate.innerHTML = moment()
+    .tz("America/Argentina/Buenos_Aires")
+    .format("LL");
+}
+
+setClock();
+setInterval(setClock, 1000);
